0142e51322688fc0048ffeace3b598b69f04831d
kauecleverson <kauecleversonr@gmail.com>
2024-09-04 00:00:50 -0300

    Pasta de revisão adicionada ao repositório da UC12

---
+++ repo-uc12/revisao-uc12-2°Semestre/1-agrupamento-de-conteudo.js
@@ -0,0 +1,17 @@
+//Esse script exibe textos escondidos em uma tab 
+
+function openTab(evt, tabName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";       
+    }
+    tabLinks = document.getElementsByTagName("li")
+    for (i = 0; i < tabLinks.length; i++){
+        tabLinks[i].className = tabLinks[i].className.replace("active", " ")
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += "active";
+}
+
+/* */
